fd1194794ccfce3cb5ec5e8689ecc643124d3476
Dániel Szoboszlay <daniel.szoboszlay@klarna.com>
2015-05-21 11:17:02 +0200

    Project skeleton

---
+++ src/mail/feeling_lucky_4_jira_incoming_mail_controller.erl
@@ -0,0 +1,8 @@
+-module(feeling_lucky_4_jira_incoming_mail_controller).
+-compile(export_all).
+
+authorize_(User, DomainName, IPAddress) ->
+    true.
+
+% post(FromAddress, Message) ->
+%    ok.
---
+++ src/view/lib/filter_modules/feeling_lucky_4_jira_custom_filters.erl
@@ -0,0 +1,9 @@
+-module(feeling_lucky_4_jira_custom_filters).
+-compile(export_all).
+
+% put custom filters in here, e.g.
+%
+% my_reverse(Value) ->
+%     lists:reverse(binary_to_list(Value)).
+%
+% "foo"|my_reverse   => "oof"
